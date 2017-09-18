c9f5b341772f2a6825053ed128e32b9fcbf0c3c1
Steven Hollingsworth <shollingsworth@barracuda.com>
2017-09-18 14:56:32 -0700

    Bugfix with pieces it's downloading

--- hrdownload.py
+++ hrdownload.py
@@ -19,13 +19,20 @@ class ScrapeHackerRank(object):
         save=False,
     ):
         self.debug = debug
+
+        hr_dir = os.environ.get("HR_DIR")
+        if not hr_dir:
+            raise Exception("Error, set HR_DIR to your hacker_rank directory as an environment variable")
         try:
             hr_name = url.split('/')[-2]
         except:
             hr_name = None
         if not hr_name:
             raise Exception("Error extracting section name from url: {}".format(url))
-        self.dirname = "{}/repos/hacker_rank/python/{}".format(os.environ.get('HOME'),hr_name)
+        if not os.path.isdir(hr_dir):
+            raise Exception("Error, cannot find hackerRank directory: {}".format(hr_dir))
+
+        self.dirname = "{}/python/{}".format(hr_dir,hr_name)
         self.match_text = match_text
         self.url = url
         self.save_file = "{}/debug.json".format(self.dirname)
@@ -42,7 +49,7 @@ class ScrapeHackerRank(object):
             self.__makeDir()
         if save:
             fh = open(self.save_file, 'w').write(json.dumps(self.doc, indent=5))
-            print("Save file initiated, exiting")
+            print("Save file initiated => {}, exiting".format(self.save_file))
             sys.exit()
 
     def __getDoc(self):
@@ -70,17 +77,21 @@ class ScrapeHackerRank(object):
     def __makeDir(self):
         if not os.path.isdir(self.dirname):
             print("Making directory: {}".format(self.dirname))
-            os.mkdir(self.dirname)
+            os.makedirs(self.dirname)
 
     def makeFiles(self):
-        code_template = self.doc.get('model',{}).get('python_template')
+        chead = self.doc.get('model',{}).get('python_template_head')
+        ctemplate = self.doc.get('model',{}).get('python_template')
+        ctail = self.doc.get('model',{}).get('python_template_tail')
+
         html = self.__getHtml()
         soup = BeautifulSoup(html,'html.parser')
         root = ET.fromstring(soup.prettify())
 
         hackdowns = []
-        for i in root.findall(".//*[@class='hackdown-content']/pre/code"):
-            hackdowns.append(i.text.strip())
+        for i in root.findall(".//*[@class='hackdown-content']/*/pre"):
+            hackdowns.append("".join([t for t in i.itertext()]))
+            #hackdowns.append(i.text.strip())
         #grab the last two elements, throw away the first
         matches = {
             'challenge_sample_output' : hackdowns.pop(),
@@ -99,19 +110,27 @@ class ScrapeHackerRank(object):
 # -*- coding: utf-8 -*-
 import __future__
 import sys
+#-- HEAD
+{}
 sys.stdin = open("./challenge_sample_input", 'r')
 
+#-- TEMPLATE
+{}
+
+#--- TAIL
 {}
 
 #-------- CUSTOM
 if __name__ == '__main__':
     s = raw_input()
     i = int(raw_input())
-    arr = map(int, raw_input().split())""".format(code_template)
+    arr = map(int, raw_input().split())
         if self.debug:
             print(fp,template_doc)
         else:
             open(fp, 'w').write(template_doc)
+""".format(chead,ctemplate,ctail)
+        open(fp, 'w').write(template_doc)
 
 """===================================================
 MAIN
@@ -121,12 +140,21 @@ if len(sys.argv) > 1:
 else:
     turl = raw_input().strip()
 
-if not turl:
-    raise Exception("Error, url is not defined!")
+#turl = "https://www.hackerrank.com/challenges/xml2-find-the-maximum-depth/problem"
+if not turl: raise Exception("Error, url is not defined!")
 s = ScrapeHackerRank(
     url=turl,
     match_text='Sample Input',
-    #save=True,
-    #debug=True,
 )
-s.makeFiles()
+try:
+    s.makeFiles()
+except:
+    #save output to test against
+    print("ERROR! --- something went wrong. Saving output")
+    s = ScrapeHackerRank(
+        url=turl,
+        match_text='Sample Input',
+        save=True,
+        #save=True,
+        #debug=True,
+    )
