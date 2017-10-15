48be061d8e5a68023f21012877b450533bb5c665
Steven Hollingsworth <shollingsworth@barracuda.com>
2017-10-14 21:46:22 -0700

    strange-advertising

---
+++ python/strange-advertising/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import __future__
+import sys
+import json
+
+def banner():
+    ban = '====' * 30
+    print("{}\nSAMPLE INP:\n{}\n{}".format(ban,ban,open(ip, 'r').read()))
+    print("{}\nSAMPLE OUT:\n{}\n{}".format(ban,ban,open(op, 'r').read()))
+    print("{}\nSTART:\n{}".format(ban,ban))
+    sys.stdin = open(ip, 'r')
+
+cnt = -1
+def comp(inp,ln):
+    outl = output_arr[ln]
+    if str(inp) != outl:
+        raise Exception("Error input output: line {}, file: {}\ngot: {} expected: {}".format(ln,op,inp,outl))
+
+
+ip = "./challenge_sample_input"
+op = "./challenge_sample_output"
+output_arr = map(str,open(op,'r').read().split('\n'))
+banner()
+# https://www.hackerrank.com/challenges/strange-advertising/problem
+
+import sys, math
+day = int(raw_input())
+people = 5
+cnt = 0
+while day > 0:
+    h = math.floor(people/2)
+    cnt += h
+    people = h * 3
+    day -= 1
+print(int(cnt))
