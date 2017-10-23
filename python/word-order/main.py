de166b64d10405adf37df39e9ef49bccb27ed40a
Steven Hollingsworth <shollingsworth@barracuda.com>
2017-10-22 18:16:59 -0700

    word-order

---
+++ python/word-order/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import __future__
+import sys
+import json
+
+def banner():
+    ban = '====' * 30
+    print("{}\nSAMPLE INP:\n{}\n{}".format(ban,ban,open(ip, 'r').read()))
+    print("{}\nSAMPLE OUT:\n{}\n{}".format(ban,ban,open(op, 'r').read()))
+    print("{}\nSTART:\n{}".format(ban,ban))
+    sys.stdin = open(ip, 'r')
+
+cnt = -1
+def comp(inp,ln):
+    outl = output_arr[ln]
+    if str(inp) != outl:
+        raise Exception("Error input output: line {}, file: {}\ngot: {} expected: {}".format(ln,op,inp,outl))
+
+
+ip = "./challenge_sample_input"
+op = "./challenge_sample_output"
+output_arr = map(str,open(op,'r').read().split('\n'))
+banner()
+# https://www.hackerrank.com/challenges/word-order/problem
+
+from collections import OrderedDict
+words = OrderedDict()
+for i in [raw_input() for _ in range(int(raw_input()))]:
+    words.setdefault(i,0)
+    words[i] += 1
+print(len(words.keys()))
+print(" ".join(map(str,words.values())))
+
