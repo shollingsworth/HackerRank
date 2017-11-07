d52f0df15d8d1a0478bbf2011c47f2a026d51457
Steven Hollingsworth <shollingsworth@barracuda.com>
2017-11-06 19:10:50 -0800

    class-2-find-the-torsional-angle

---
+++ python/class-2-find-the-torsional-angle/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import __future__
+import sys
+import json
+
+def banner():
+    ban = '====' * 30
+    print("{}\nSAMPLE INP:\n{}\n{}".format(ban,ban,open(ip, 'r').read()))
+    print("{}\nSAMPLE OUT:\n{}\n{}".format(ban,ban,open(op, 'r').read()))
+    print("{}\nSTART:\n{}".format(ban,ban))
+    sys.stdin = open(ip, 'r')
+
+cnt = -1
+def comp(inp,ln):
+    outl = output_arr[ln]
+    if str(inp) != outl:
+        raise Exception("Error input output: line {}, file: {}\ngot: {} expected: {}".format(ln,op,inp,outl))
+
+
+ip = "./challenge_sample_input"
+op = "./challenge_sample_output"
+output_arr = map(str,open(op,'r').read().split('\n'))
+banner()
+# https://www.hackerrank.com/challenges/class-2-find-the-torsional-angle/problem
+
+import math
+class Points(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __sub__(self, no):
+        return Points(
+            self.x - no.x,
+            self.y - no.y,
+            self.z - no.z,
+        )
+
+    def dot(self, no):
+        return self.x*no.x + self.y*no.y + self.z*no.z
+
+    def cross(self, no):
+        return Points(
+            self.y*no.z - self.z*no.y,
+            self.z*no.x - self.x*no.z,
+            self.x*no.y - self.y*no.x
+        )
+        
+    def absolute(self):
+        return pow((self.x ** 2 + self.y ** 2 + self.z ** 2), 0.5)
+
+if __name__ == '__main__':
+    points = [map(float, raw_input().split()) for i in range(4)]
+    a, b, c, d = Points(*points[0]), Points(*points[1]), Points(*points[2]), Points(*points[3])
+    x = (b - a).cross(c - b)
+    y = (c - b).cross(d - c)
+    angle = math.acos(x.dot(y) / (x.absolute() * y.absolute()))
+    print "%.2f" % math.degrees(angle)
