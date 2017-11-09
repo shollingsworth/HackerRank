629d39bbf58002ea97aa7ab9d48e8b3412775c3a
Steven Hollingsworth <shollingsworth@barracuda.com>
2017-11-08 17:47:36 -0800

    python-power-mod-power

---
+++ python/python-power-mod-power/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import __future__
+import sys
+import json
+
+def banner():
+    ban = '====' * 30
+    print("{}\nSAMPLE INP:\n{}\n{}".format(ban,ban,open(ip, 'r').read()))
+    print("{}\nSAMPLE OUT:\n{}\n{}".format(ban,ban,open(op, 'r').read()))
+    print("{}\nSTART:\n{}".format(ban,ban))
+    sys.stdin = open(ip, 'r')
+
+cnt = -1
+def comp(inp,ln):
+    outl = output_arr[ln]
+    if str(inp) != outl:
+        raise Exception("Error input output: line {}, file: {}\ngot: {} expected: {}".format(ln,op,inp,outl))
+
+
+ip = "./challenge_sample_input"
+op = "./challenge_sample_output"
+output_arr = map(str,open(op,'r').read().split('\n'))
+banner()
+# https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+a,b,m = [ int(raw_input()) for _ in range(3) ]
+print("\n".join(map(str,[ pow(a,b), pow(a,b,m) ])))
