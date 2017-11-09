ab88ae541917e573f54840514546b53eb55fce9d
Steven Hollingsworth <shollingsworth@barracuda.com>
2017-11-08 17:35:30 -0800

    triangle-quest-2 & #1
    These were tough at first

---
+++ python/python-quest-1/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import __future__
+import sys
+import json
+
+def banner():
+    ban = '====' * 30
+    print("{}\nSAMPLE INP:\n{}\n{}".format(ban,ban,open(ip, 'r').read()))
+    print("{}\nSAMPLE OUT:\n{}\n{}".format(ban,ban,open(op, 'r').read()))
+    print("{}\nSTART:\n{}".format(ban,ban))
+    sys.stdin = open(ip, 'r')
+
+cnt = -1
+def comp(inp,ln):
+    outl = output_arr[ln]
+    if str(inp) != outl:
+        raise Exception("Error input output: line {}, file: {}\ngot: {} expected: {}".format(ln,op,inp,outl))
+
+
+ip = "./challenge_sample_input"
+op = "./challenge_sample_output"
+output_arr = map(str,open(op,'r').read().split('\n'))
+banner()
+# https://www.hackerrank.com/challenges/python-quest-1/problem
+
+for i in range(1,input()): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((i*(10**i -1)/9))
