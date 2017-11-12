a00a6fdfb4ea409a57772fc635faa87550669b99
Steven Hollingsworth <shollingsworth@barracuda.com>
2017-11-11 18:20:10 -0800

    re-start-re-end

---
+++ python/re-start-re-end/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import sys
+import json
+
+def banner():
+    ban = '====' * 30
+    print("{}\nSAMPLE INP:\n{}\n{}".format(ban,ban,open(ip, 'r').read()))
+    print("{}\nSAMPLE OUT:\n{}\n{}".format(ban,ban,open(op, 'r').read()))
+    print("{}\nSTART:\n{}".format(ban,ban))
+    sys.stdin = open(ip, 'r')
+
+cnt = -1
+def comp(inp,ln):
+    outl = output_arr[ln]
+    if str(inp) != outl:
+        raise Exception("Error input output: line {}, file: {}\ngot: {} expected: {}".format(ln,op,inp,outl))
+
+
+ip = "./challenge_sample_input"
+op = "./challenge_sample_output"
+output_arr = map(str,open(op,'r').read().split('\n'))
+banner()
+# https://www.hackerrank.com/challenges/re-start-re-end/problem
+
+import re
+s,k = [ raw_input() for i in range(2) ]
+a = [ (m.start(), m.start() + len(k) - 1) for m in re.finditer(r'(?=({}))'.format(k),s) ]
+print("\n".join(map(str,a)) if len(a) else (-1,-1))
